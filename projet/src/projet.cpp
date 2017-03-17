6909000a2dd19faf1ba3582157d5d179e5cf36d7
zinaEL <zina.guedria@gmail.com>
2017-03-17 11:52:07 +0100

    ajout un affichage

---
+++ projet/src/projet.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : projet.cpp
+// Author      : Zina EL Guedria
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout <<"premiere modification";
+	return 0;
+}
